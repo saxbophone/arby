f9b2fb1ddaf81d3ea88edb7ae6a024e31a3a8e40
Joshua Saxby <joshua.a.saxby@gmail.com>
2022-05-08 03:34:53 +0100

    First working implementation of stringification

--- arby/src/arby.cpp
+++ arby/src/arby.cpp
@@ -5,6 +5,8 @@
  */
 
 #include <cstdint>
+
+#include <iostream>
 #include <string>
 
 #include <arby/arby.hpp>
@@ -18,6 +20,26 @@ namespace com::saxbophone::arby {
     }
 
     std::ostream& operator<<(std::ostream& os, const Uint& object) {
+        // this will store the leftover bits
+        Uint value = object;
+        // find the largest power of 10 that is just about not greater than value
+        Uint power = 1;
+        while (value > power * 10) {
+            power *= 10;
+        }
+        // std::cout << (uintmax_t)power << std::endl;
+        // now peel the digits off one by one
+        while (power > 0) {
+            auto [quotient, remainder] = Uint::divmod(value, power);
+            // quotient should be in the range 0..9 and therefore only 1-digit
+            if (quotient._digits.size() == 0) {
+                os << '0';
+            } else {
+                os << std::to_string(quotient._digits.front());
+            }
+            value = remainder;
+            power /= 10;
+        }
         return os;
     }
 }
