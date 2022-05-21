0e5c2439ed70f8ee564174a29cbe7ae0f80fe3fa
Joshua Saxby <joshua.a.saxby@gmail.com>
2022-05-21 23:33:06 +0100

    I went so far in implementing it, then realised there's no bin for ios!

--- arby/src/arby.cpp
+++ arby/src/arby.cpp
@@ -19,13 +19,11 @@ namespace com::saxbophone::arby {
       : _digits(operator "" _uarb(digits.c_str())._digits)
       {}
 
-    std::string Uint::_stringify_for_base() const {
-        // XXX: only base-10 implemented for now
-        // TODO: add a param to specify base when we implement other bases
+    std::string Uint::_stringify_for_base(std::uint8_t base) const {
         Uint value = *this;
         std::string digits;
         do {
-            auto [quotient, remainder] = Uint::divmod(value, 10);
+            auto [quotient, remainder] = Uint::divmod(value, base);
             if (remainder._digits.size() == 0) {
                 digits += '0';
             } else {
@@ -38,11 +36,23 @@ namespace com::saxbophone::arby {
     }
 
     std::ostream& operator<<(std::ostream& os, const Uint& object) {
-        os << object._stringify_for_base();
+        // the implementation of std::dec, std::hex and std::bin guarantees that
+        // only one of them will be set in the IO stream flags if the proper
+        // stdlib function is used to set those flags
+        // we test for hex, bin, then fallback to dec
+        uint8_t base = 10;
+        if (os.flags() & std::ios::hex) {
+            // ...
+        }
+        // XXX: whoops! there is no ios::bin! only ::oct!
+        // else if (os.flags() & std::ios::bin) { 
+            // ...
+        // }
+        os << object._stringify_for_base(10);
         return os;
     }
 
     Uint::operator std::string() const {
-        return this->_stringify_for_base();
+        return this->_stringify_for_base(10);
     }
 }
