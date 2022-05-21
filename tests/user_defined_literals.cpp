68ca4337f0fce883d9f403710b0bda6ed9f9ac2e
Joshua Saxby <joshua.a.saxby@gmail.com>
2022-05-21 01:37:18 +0100

    Wrote failing tests for literals of other number bases

--- tests/user_defined_literals.cpp
+++ tests/user_defined_literals.cpp
@@ -9,7 +9,7 @@
 
 using namespace com::saxbophone::arby;
 
-TEST_CASE("Initialising arby::Uint objects with _uarb user-defined-literal", "[user-defined-literal]") {
+TEST_CASE("Initialising arby::Uint objects with decimal _uarb user-defined-literal", "[user-defined-literal]") {
     auto [literal, object] = GENERATE(
         table<Uint, Uint>(
             {
@@ -22,6 +22,41 @@ TEST_CASE("Initialising arby::Uint objects with _uarb user-defined-literal", "[u
                 {123456_uarb, 123456},
                 {9999999_uarb, 9999999},
                 {56213870_uarb, 56213870},
+            }
+        )
+    );
+
+    CHECK(literal == object);
+}
+
+TEST_CASE("Initialising arby::Uint objects with hexadecimal _uarb user-defined-literal", "[user-defined-literal]") {
+    auto [literal, object] = GENERATE(
+        table<Uint, Uint>(
+            {
+                {0x1172443_uarb, 0x1172443},
+                {0x19721871_uarb, 0x19721871},
+                {0x0_uarb, 0x0},
+                {0xf3c1d28a_uarb, 0xf3c1d28a},
+                {0x01234567_uarb, 0x01234567},
+                {0x89abcdef_uarb, 0x89abcdef},
+                {0xffffffff_uarb, 0xffffffff},
+                {0xf00000e1_uarb, 0xf00000e1},
+            }
+        )
+    );
+
+    CHECK(literal == object);
+}
+
+TEST_CASE("Initialising arby::Uint objects with binary _uarb user-defined-literal", "[user-defined-literal]") {
+    auto [literal, object] = GENERATE(
+        table<Uint, Uint>(
+            {
+                {0b0_uarb, 0b0},
+                {0b1_uarb, 0b1},
+                {0b11010010_uarb, 0b11010010},
+                {0b1111111111111111111111111111_uarb, 0b1111111111111111111111111111},
+                {0b1010011010100011110101110101_uarb, 0b1010011010100011110101110101},
             }
         )
     );
