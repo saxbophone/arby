61ca8e820aab17b20ce3ec8e85cb6db72fe3c970
Joshua Saxby <joshua.a.saxby@gmail.com>
2022-05-26 00:11:35 +0100

    Wrote failing tests for the actual ilog() function

--- tests/math_support/ilog.cpp
+++ tests/math_support/ilog.cpp
@@ -3,6 +3,7 @@
 
 #include <limits>
 #include <stdexcept>
+#include <utility>
 
 #include <catch2/catch.hpp>
 
@@ -19,4 +20,39 @@ TEST_CASE("arby::ilog(base<2, nonzero) throws std::domain_error", "[math-support
 
 TEST_CASE("arby::ilog(base>1, 0) throws std::domain_error", "[math-support][ilog]") {
     CHECK_THROWS_AS(arby::ilog(2_nat, 0_nat), std::domain_error);
+}
+
+// uses the C++ stdlib to take a short-cut in calculating expected results in tests
+static std::pair<uintmax_t, uintmax_t> integer_log(uintmax_t base, uintmax_t x) {
+    // use the log/log trick to get any base log
+    long double l = std::log(x) / std::log(base);
+    // take the floor and ceiling, cast to integer
+    return {
+        (uintmax_t)std::floor(l), (uintmax_t)std::ceil(l)
+    };
+}
+
+TEST_CASE("arby::ilog(base>1, nonzero) with small base", "[math-support][ilog]") {
+    // don't use a base that's too small to get a variety of results
+    uintmax_t base = GENERATE(take(1000, random((uintmax_t)2, (uintmax_t)1000)));
+    uintmax_t x = GENERATE(take(1000, random((uintmax_t)1, std::numeric_limits<uintmax_t>::max())));
+    auto [expected_floor, expected_ceil] = integer_log(base, x);
+
+    auto [result_floor, result_ceil] = arby::ilog(base, x);
+
+    CAPTURE(base, x);
+    CHECK(result_floor == expected_floor);
+    CHECK(result_ceil == expected_ceil);
+}
+
+TEST_CASE("arby::ilog(base>1, nonzero)", "[math-support][ilog]") {
+    uintmax_t base = GENERATE(take(1000, random((uintmax_t)2, std::numeric_limits<uintmax_t>::max())));
+    uintmax_t x = GENERATE(take(1000, random((uintmax_t)1, std::numeric_limits<uintmax_t>::max())));
+    auto [expected_floor, expected_ceil] = integer_log(base, x);
+
+    auto [result_floor, result_ceil] = arby::ilog(base, x);
+
+    CAPTURE(base, x);
+    CHECK(result_floor == expected_floor);
+    CHECK(result_ceil == expected_ceil);
 }
