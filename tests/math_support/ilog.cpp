e52b5ca8adca0bbfd065427106cf572b74756fce
Joshua Saxby <joshua.a.saxby@gmail.com>
2022-05-25 23:51:10 +0100

    Tweak test case tags

--- tests/math_support/ilog.cpp
+++ tests/math_support/ilog.cpp
@@ -12,11 +12,11 @@
 using namespace com::saxbophone;
 using namespace com::saxbophone::arby::literals;
 
-TEST_CASE("arby::ilog(base<2, nonzero) throws std::domain_error") {
+TEST_CASE("arby::ilog(base<2, nonzero) throws std::domain_error", "[math-support][ilog]") {
     arby::Nat base = GENERATE(0_nat, 1_nat);
     CHECK_THROWS_AS(arby::ilog(base, 1), std::domain_error);
 }
 
-TEST_CASE("arby::ilog(base>1, 0) throws std::domain_error") {
+TEST_CASE("arby::ilog(base>1, 0) throws std::domain_error", "[math-support][ilog]") {
     CHECK_THROWS_AS(arby::ilog(2_nat, 0_nat), std::domain_error);
 }
