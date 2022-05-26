cd05542eaba974555682ecd7c9e0f8a39ba7740c
Joshua Saxby <joshua.a.saxby@gmail.com>
2022-05-26 13:50:51 +0100

    Fixed a mistake in tests: forgot to use arby::Nat literals!

--- tests/Nat/bitwise.cpp
+++ tests/Nat/bitwise.cpp
@@ -7,6 +7,7 @@
 #include <arby/Nat.hpp>
 
 using namespace com::saxbophone;
+using namespace com::saxbophone::arby::literals;
 
 TEST_CASE("arby::Nat bitwise assignment-OR", "[bitwise]") {
     uintmax_t value = GENERATE(take(200, random((uintmax_t)0, std::numeric_limits<uintmax_t>::max())));
@@ -52,9 +53,9 @@ TEST_CASE("arby::Nat bitwise OR with hardcoded values", "[bitwise]") {
     auto [lhs, rhs, expected] = GENERATE(
         table<arby::Nat, arby::Nat, arby::Nat>(
             {
-                {0b1101000001001010111111001, 0b00000011111111, 0b1101000001001010111111111},
-                {0x637981823345789012923acbde4184921008, 0x93f393c3e3d3a34c4c9420000, 0x63798182334d7fb93ebe3ffbfec5cdd21008},
-                {10226483191214161820, 112, 10226483191214161916}
+                {0b1101000001001010111111001_nat, 0b00000011111111_nat, 0b1101000001001010111111111_nat},
+                {0x637981823345789012923acbde4184921008_nat, 0x93f393c3e3d3a34c4c9420000_nat, 0x63798182334d7fb93ebe3ffbfec5cdd21008_nat},
+                {10226483191214161820_nat, 112_nat, 10226483191214161916_nat}
             }
         )
     );
@@ -108,9 +109,9 @@ TEST_CASE("arby::Nat bitwise AND with hardcoded values", "[bitwise]") {
     auto [lhs, rhs, expected] = GENERATE(
         table<arby::Nat, arby::Nat, arby::Nat>(
             {
-                {0b1101000001001010111111001, 0b00000011111111, 0b11111001},
-                {0x637981823345789012923acbde4184921008, 0x93f393c3e3d3a34c4c9420000, 0x138101012380a144080020000},
-                {10226483191214161820, 112, 16}
+                {0b1101000001001010111111001_nat, 0b00000011111111_nat, 0b11111001_nat},
+                {0x637981823345789012923acbde4184921008_nat, 0x93f393c3e3d3a34c4c9420000_nat, 0x138101012380a144080020000_nat},
+                {10226483191214161820_nat, 112_nat, 16_nat}
             }
         )
     );
@@ -165,9 +166,9 @@ TEST_CASE("arby::Nat bitwise XOR with hardcoded values", "[bitwise]") {
     auto [lhs, rhs, expected] = GENERATE(
         table<arby::Nat, arby::Nat, arby::Nat>(
             {
-                {0b1101000001001010111111001, 0b00000011111111, 0b1101000001001010100000110},
-                {0x637981823345789012923acbde4184921008, 0x93f393c3e3d3a34c4c9420000, 0x63798182334c47a92eac07f1ea854dd01008},
-                {10226483191214161820, 112, 10226483191214161900}
+                {0b1101000001001010111111001_nat, 0b00000011111111_nat, 0b1101000001001010100000110_nat},
+                {0x637981823345789012923acbde4184921008_nat, 0x93f393c3e3d3a34c4c9420000_nat, 0x63798182334c47a92eac07f1ea854dd01008_nat},
+                {10226483191214161820_nat, 112_nat, 10226483191214161900_nat}
             }
         )
     );
