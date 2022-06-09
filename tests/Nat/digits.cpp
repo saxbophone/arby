8688ccf5b78c3273547fb9fcd232b50e3f5a7a8a
Joshua Saxby <joshua.a.saxby@gmail.com>
2022-06-10 00:19:47 +0100

    Added failing test cases for init from vector and initializer_list For some reason, the product test case one doesn't compile unless sign-conversion warnings are disabled. Not clear why...

--- tests/Nat/digits.cpp
+++ tests/Nat/digits.cpp
@@ -9,7 +9,28 @@
 
 using namespace com::saxbophone;
 
-TEST_CASE("Nat digits") {
+TEST_CASE("Nat digits - std::initializer_list", "") {
+    std::initializer_list<arby::Nat::StorageType> digits = {
+        1u, 2u, 3u, 4u, 5u, 6u, 7u, 8u
+    };
+    // also put those digits into a codlili::List instance so we can compare them
+    codlili::List<arby::Nat::StorageType> original(digits);
+
+    SECTION("Create Nat from digits") {
+        arby::Nat from_digits(digits);
+
+        SECTION("Get digits back out of Nat") {
+            auto digits_back = from_digits.digits();
+
+            CHECK(digits_back == original);
+        }
+    }
+}
+
+TEMPLATE_PRODUCT_TEST_CASE(
+    "Nat digits", "",
+    (codlili::List, std::vector), arby::Nat::StorageType
+) {
     // generate digits arrays of size 1..8
     std::size_t size = GENERATE(take(1000, random(1u, 8u)));
     auto raw_digits = GENERATE_COPY(
@@ -18,16 +39,21 @@ TEST_CASE("Nat digits") {
             chunk(
                 size,
                 random(
-                    (arby::Nat::StorageType)0,
+                    (arby::Nat::StorageType)1,
                     std::numeric_limits<arby::Nat::StorageType>::max()
                 )
             )
         )
     );
-    // copy raw_digits into a codlili::List
-    codlili::List<arby::Nat::StorageType> digits;
+    // copy raw_digits into a container of TestType
+    TestType digits;
     for (const auto& item : raw_digits) {
         digits.push_back(item);
+    }
+    // also read digits out into a codlili::List instance so we can compare them
+    codlili::List<arby::Nat::StorageType> original;
+    for (const auto& item : digits) {
+        original.push_back(item);
     }
 
     SECTION("Create Nat from digits") {
@@ -36,7 +62,7 @@ TEST_CASE("Nat digits") {
         SECTION("Get digits back out of Nat") {
             auto digits_back = from_digits.digits();
 
-            CHECK(digits_back == digits);
+            CHECK(digits_back == original);
         }
     }
 }
