6a1ef1f5438b80df5e1c745e58c49028967ea237
Joshua Saxby <joshua.a.saxby@gmail.com>
2023-02-18 15:36:00 +0000

    You can recursively multiply with bit shifts in this way, it's not efficient though It's actually much slower when done this way, by a factor of ~8

--- arby/include/arby/Nat.hpp
+++ arby/include/arby/Nat.hpp
@@ -576,6 +576,13 @@ namespace com::saxbophone::arby {
                 return lhs << (rhs.bit_length() - 1);
             } else if (lhs.is_power_of_2()) {
                 return rhs * lhs;
+            } else {
+                // recursive implementation
+                auto big = lhs > rhs ? lhs : rhs;
+                auto small = lhs < rhs ? lhs : rhs;
+                auto current_power = arby::Nat(1) << (big.bit_length() - 1);
+                auto left_over = big - current_power;
+                return small * current_power + small * left_over;
             }
             // multiply each digit from lhs with each digit from rhs
             std::size_t l = 0; // manual indices to track which digit we are on,
